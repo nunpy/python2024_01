bfed150c6260f67b902c2a257be28a0d9255b5a2
nunpy <155503328+nunpy@users.noreply.github.com>
2024-01-03 12:50:27 +0900

    Add files via upload

---
+++ Day1/quiz.py
@@ -0,0 +1,20 @@
+# 자기소개
+# 이름 입력
+# mbti 입력
+# 좋아하는 음식 입력:
+#결과: 이름이 ~~ 이시고,
+
+name = ("이채민")
+mbti = ("infj")
+favorite_food = ("라구파스타")
+
+name = input("당신의 이름은:")
+mbti = input("mbti는:")
+favorite_food = input("최애 음식:")
+
+print(f"당신의 이름은 {name}이고, mbti는 {mbti}이시고, 최애 음식은 {favorite_food}이군요!😍")
+
+#좀더알아보기
+movie = input("최근에 본 영화 입력:")
+ott = input("최근에 본 OTT 시리즈 입력:")
+print(f"당신이 최근에 본 영화는 {movie}이고, 최근에 본 OTT 시리즈는 {ott}이군요!😎")
